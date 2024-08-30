310daed784f68e2f44a8a8a17bcedb1bd70b1ca9
riyasoner <124883022+riyasoner@users.noreply.github.com>
2024-08-30 12:52:03 +0530

    Create authenticate.js

---
+++ authenticate.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken" ;
+export const verify = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).json({
+        status: 'fail',
+        message: 'Unauthorized!no token',
+      });
+  }
+  try {
+    const user = jwt.verify(authHeader, 'secretkey');
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({
+        status: 'fail',
+        message: 'Unauthorized!',
+      });
+  }
+};
